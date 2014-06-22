48dcdc6bd36e7793d16d10c2d87c0a320c435d22
Ed Swindelles <ed@swindelles.com>
2014-06-21 21:39:12 -0400

    Added README, cleaned up script

---
+++ README.md
@@ -0,0 +1,39 @@
+# Overview
+
+This R script is prepared for the Getting and Cleaning Data course project in the Johns Hopkins 
+University Bloomberg School of Public Health Data Science specialization on Coursera.
+
+* Version: 1.0
+* Submitted: 6/21/2014
+* Author: Ed Swindelles
+** http://github.com/edswindelles
+** http://twitter.com/edswindelles
+
+# Purpose
+
+The purpose of this script is to produce a tidy dataset (1)(2) from the Human Activity 
+Recognition Using Smartphones Dataset (SmartLab at the University of Genova).  The dataset
+is preprocessed, but spread across multiple files, with no data labeling or summarization.  
+The dataset is split into two groups, test data (30% of total) and training data (70% of 
+total).  This script combines the dataset files, summarizes, and labels.
+
+# Usage
+
+The script assumes that the current working directory is the extracted original dataset.
+
+	setwd("/work/UCI HAR Dataset")
+	source("/work/run_analysis.R")
+
+The output of the script is three text files, each more tidy than the previous.
+
+* __tidy_data1.txt__ - All records from both the test and training datasets in tidy format.
+* __tidy_data2.txt__ - _tidy_data1_, with only columns pertaining to std() and mean() 
+calculations.
+* __tidy_data3.txt__ - _tidy_data2_, with only 1 row per subject/activity pairing, and the 
+mean() of each column.
+
+## References
+(1) Wickham, H. (submitted). Tidy Data. Journal of Statistical Software.  Retrieved from 
+http://vita.had.co.nz/papers/tidy-data.pdf
+(2) Hood, D. (2014, June 14). Long Data, Wide Data, and Tidy Data for the Assignment.
+Retrieved from https://class.coursera.org/getdata-004/forum/thread?thread_id=262
--- run_analysis.R
+++ run_analysis.R
@@ -1,31 +1,51 @@
-NROWS <- 1
+library("data.table")
 
+# Set NROWS constant to -1 to process all rows in each file, or a positive value for a subset of rows.
+NROWS <- 100
+
+# Helper function to ensure that each file is read in the same manner, since they all have the same format.
 read.num <- function(f) { read.table(f, colClasses = numeric(), header = FALSE, comment.char = "", nrows = NROWS) }
 
+# Load activity labels and friendly column names for tidy output
 activitylabels <- read.table("activity_labels.txt")
 cols <- c("subject", "activity", as.character(read.table("features.txt")$V2))
 
-tidy1 <- cbind(
+# Load the test data
+temp1 <- cbind(
+			# Subjects
 			read.num("test/subject_test.txt"),
+			# Activities, substituting the character name for the numeric value
 			sapply(read.num("test/y_test.txt"), function(x) { activitylabels[x, 2] }),
+			# Data
 			read.num("test/X_test.txt")
-)
+		 )
 
-tidy2 <- cbind(
+# Load the training data
+temp2 <- cbind(
+			# Subjects
 			read.num("train/subject_train.txt"),
+			# Activities, substituting the character name for the numeric value
 			sapply(read.num("train/y_train.txt"), function(x) { activitylabels[x, 2] }),
+			# Data
 			read.num("train/X_train.txt")
-)
+		 )
 
-colnames(tidy1) <- colnames(tidy2) <- cols
+# Replace the generic columns names with friendly names
+colnames(temp1) <- colnames(temp2) <- cols
 
-tidy <- rbind(tidy1, tidy2)
-tidy <- tidy[order(tidy$subject, tidy$activity), ]
+#Combine the test and training data sets, then order by subject and activity
+tidy1 <- rbind(temp1, temp2)
+tidy1 <- tidy1[order(tidy1$subject, tidy1$activity), ]
 
-row.names(tidy) <- NULL
+# Reset the row names to be sequential for tidy output
+row.names(tidy1) <- NULL
 
-write.table(tidy, "tidy_data.txt", row.names = FALSE)
+# Subset the tidy data by selecting just columns pertaining to std() and mean() calculations
+tidy2 <- tidy1[, sapply(colnames(tidy1), function(c) { grepl("^subject$|^activity$|std|mean[^Freq]", c) }, USE.NAMES = FALSE)]
+# Group the tidy2 data by subject and activity with the mean for each data 
+tidy3 <- data.table(tidy2)[, lapply(.SD, mean), by = c("subject", "activity")]
 
-subtidy <- tidy[, sapply(colnames(tidy), function(c) { grepl("^subject$|^activity$|std|mean[^Freq]", c) }, USE.NAMES = FALSE)]
-
-write.table(subtidy, "tidy_data_mean_std.txt", row.names = FALSE)
+# Write tidy data sets out to files
+write.table(tidy1, "tidy_data1.txt", row.names = FALSE)
+write.table(tidy2, "tidy_data2.txt", row.names = FALSE)
+write.table(tidy3, "tidy_data3.txt", row.names = FALSE)
